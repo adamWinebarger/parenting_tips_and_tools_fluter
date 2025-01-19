6eb6aa7f0b3c90532fff06c78bbc6b6501ca77c3
adamWinebarger <adam.k.winebarger@gmail.com>
2025-01-19 18:31:33 -0500

    Map Widget now works on iphone. Not sure about android

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,11 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class LocationScreen extends StatelessWidget {
-  LocationScreen({super.key});
+class LocationScreen extends StatefulWidget {
+  @override
+  State<LocationScreen> createState() => _LocationScreenState();
 
-  final _officeCoords = LatLng(43.0617955, -86.228449);
+}
+
+class _LocationScreenState extends State<LocationScreen> {
+  //LocationScreen({super.key});
+
+  final _officeCoords = const LatLng(43.0617955, -86.228449);
+
+  bool _hasNavigationPermission = false;
+
+  Future<void> _checkNavigationPermissions() async {
+    final status = await Permission.location.status;
+    if (status.isGranted) {
+      setState(() {
+        _hasNavigationPermission = true;
+      });
+    }
+  }
+
+  Future<void> _requestPermission() async {
+    final status = await Permission.location.request();
+    if (status.isGranted) {
+      setState(() {
+        _hasNavigationPermission = true;
+      });
+    }
+
+    if (status.isPermanentlyDenied) {
+      openAppSettings();
+    }
+  }
 
   void _openNavigationApp() async {
     final googleMapsURL = Uri.parse(
@@ -19,18 +50,25 @@ class LocationScreen extends StatelessWidget {
   }
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _checkNavigationPermissions();
+  }
+
+  @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(title: Text("Location"),),
       body: Container(
-        margin: EdgeInsets.all(5),
+        margin: EdgeInsets.all(25),
         padding: EdgeInsets.symmetric(vertical: 45, horizontal: 10),
         child: Center(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text(
+              const Text(
                 "Our Office Location",
                 style: TextStyle(
                   fontSize: 16,
@@ -38,30 +76,21 @@ class LocationScreen extends StatelessWidget {
                 ),
                 textAlign: TextAlign.center,
               ),
-              SizedBox(height: 25,),
-              Expanded(
-                  child: GoogleMap(
-                    initialCameraPosition: CameraPosition(
-                        target: _officeCoords,
-                        zoom: 16.0
-                    ),
-                    markers: {
-                      Marker(
-                          markerId: MarkerId("Dr. Al's Office"),
-                          position: _officeCoords,
-                          infoWindow: InfoWindow(title: "Dr. Al's Office")
-                      ),
-                    },
-                    myLocationButtonEnabled: false,
-                    myLocationEnabled: false,
-                    zoomControlsEnabled: false, //might come back and change this one
-                    scrollGesturesEnabled: false,
-                    tiltGesturesEnabled: false,
-                    rotateGesturesEnabled: false,
-                    zoomGesturesEnabled: false,
-                  ),
-              ),
+              const SizedBox(height: 25,),
+              //_hasNavigationPermission ? _googleMapsWidget() : _buildErrorWidget(),
+              _googleMapsWidget(),
               SizedBox(height: 10),
+              TextButton(
+                onPressed: _openNavigationApp,
+                child: const Text(
+                  "509 Franklin Ave.,\nGrand Haven, MI, 49456",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white
+                  ),
+                  textAlign: TextAlign.center,
+                )
+              )
               // GestureDetector(
               //   onDoubleTap: _openNavigationApp,
               //   child: Container(
@@ -78,7 +107,56 @@ class LocationScreen extends StatelessWidget {
           ),
         ),
       )
+    );
+  }
 
+  Widget _googleMapsWidget() {
+    return Expanded(
+      child: GestureDetector(
+        onDoubleTap: _openNavigationApp,
+        child: GoogleMap(
+          initialCameraPosition: CameraPosition(
+              target: _officeCoords,
+              zoom: 16.0
+          ),
+          markers: {
+            Marker(
+                markerId: MarkerId("Dr. Al's Office"),
+                position: _officeCoords,
+                infoWindow: InfoWindow(title: "Dr. Al's Office")
+            ),
+          },
+          myLocationButtonEnabled: false,
+          myLocationEnabled: false,
+          zoomControlsEnabled: false, //might come back and change this one
+          scrollGesturesEnabled: false,
+          tiltGesturesEnabled: false,
+          rotateGesturesEnabled: false,
+          zoomGesturesEnabled: false,
+        ),
+      )
+
+    );
+  }
+
+  Widget _buildErrorWidget() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Location permission is required to show the map.',
+            style: TextStyle(
+              fontSize: 16
+            ),
+            textAlign: TextAlign.center,
+          ),
+          TextButton(
+            onPressed: _requestPermission,
+            child: Text("Want to enable location permissions?", style: TextStyle(fontSize: 16), textAlign: TextAlign.center,)
+          )
+        ],
+      ),
     );
   }
 
